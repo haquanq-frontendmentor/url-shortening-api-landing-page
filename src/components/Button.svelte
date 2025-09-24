8023e4e761e3a273001da626fca2955c897c3770
Ha Quanq <quangha0120010@gmail.com>
2025-09-24 18:01:34 +0700

    Add prop for Button: click event

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,15 +3,16 @@
   import type { HTMLButtonAttributes } from "svelte/elements";
   import { cn } from "../utils/cn";
 
-  interface ButtonProps {
+  type ButtonProps = {
     children?: Snippet;
     type?: HTMLButtonAttributes["type"];
     link?: string;
     xClass?: string;
     square?: boolean;
-  }
+    onClick?: (e: Event) => void;
+  };
 
-  const { square, children, type = "button", link, xClass }: ButtonProps = $props();
+  const { square, children, type = "button", link, xClass, onClick }: ButtonProps = $props();
   const className = cn(
     "bg-blue-400 rounded-full px-10 py-[1.125rem] text-xl font-bold text-white leading-none block hover:opacity-70 transition-opacity cursor-pointer",
     {
@@ -20,8 +21,8 @@
   );
 </script>
 
-{#if link != undefined}
-  <a class={cn(className, xClass)} href={link}>{@render children?.()}</a>
+{#if link !== undefined}
+  <a class={cn(className, xClass)} href={link} onclick={onClick}>{@render children?.()}</a>
 {:else}
-  <button class={cn(className, xClass)} {type}>{@render children?.()}</button>
+  <button class={cn(className, xClass)} {type} onclick={onClick}>{@render children?.()}</button>
 {/if}
