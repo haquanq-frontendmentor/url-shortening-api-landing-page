aebc79b0ba509922df5b13fe1539d301123c0cb1
Ha Quanq <quangha0120010@gmail.com>
2025-10-11 09:05:33 +0700

    Wrap Nav inside header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -51,7 +51,7 @@
   };
 </script>
 
-<nav aria-label="Header navigation">
+<header>
   <Container>
     <div class="flex items-center justify-between pt-10 pb-6 md:pt-12 md:pl-[0.125rem]">
       <a class="z-20 mt-2" href="/" aria-label="Homepage">
@@ -63,44 +63,46 @@
           /></svg
         >
       </a>
-      <button
-        class="peer group z-20 flex min-h-[2.0625rem] cursor-pointer flex-col items-center justify-center gap-1.5 *:h-[0.1875rem] *:w-6 *:shrink-0 *:bg-gray-950 *:transition-transform aria-expanded:gap-0 aria-expanded:*:bg-white md:hidden"
-        type="button"
-        aria-label="Mobile menu"
-        aria-expanded={menuOpening}
-        aria-controls="mobile-menu"
-        use:menuButtonAction
-      >
-        <span class="group-aria-expanded:-mb-[0.1875rem] group-aria-expanded:rotate-45"></span>
-        <span class="group-aria-expanded:hidden"></span>
-        <span class="group-aria-expanded:-rotate-45"></span>
-      </button>
-      <div
-        class="absolute inset-0 hidden items-start justify-center overflow-y-scroll bg-[hsla(0,0%,0%,0.5)] pt-24 pb-6 peer-aria-expanded:flex md:static md:block md:w-full md:overflow-auto md:bg-transparent md:p-0"
-        id="mobile-menu"
-        use:menuAction
-      >
-        <div class="w-[min(100vw-3rem,25rem)] rounded-lg bg-purple-950 px-6 py-10 md:w-full md:bg-transparent md:p-0">
-          <ul
-            class="flex h-fit flex-col gap-8 text-center text-lg leading-[1.5625rem] font-bold text-white *:shrink-0 *:transition-colors md:flex-row md:items-center md:pl-[2.8125rem] md:text-[0.9375rem] md:leading-none md:text-gray-500 md:*:hover:text-gray-950"
-            role="list"
-          >
-            <li><a href="/">Features</a></li>
-            <li><a href="/">Pricing</a></li>
-            <li><a href="/">Resources</a></li>
-            <li class="border-b border-gray-500 md:grow md:border-none" role="none"></li>
-            <li><a href="/">Login</a></li>
-            <li>
-              <Button
-                link="/"
-                xClass=" pt-4 pb-[0.875rem] text-lg md:text-[0.9375rem] leading-none md:pt-3 md:pb-[0.8125rem] md:px-6"
-              >
-                Sign Up
-              </Button>
-            </li>
-          </ul>
+      <nav class="md:w-full" aria-label="Header navigation">
+        <button
+          class="peer group z-20 flex min-h-[2.0625rem] cursor-pointer flex-col items-center justify-center gap-1.5 *:h-[0.1875rem] *:w-6 *:shrink-0 *:bg-gray-950 *:transition-transform aria-expanded:gap-0 aria-expanded:*:bg-white md:hidden"
+          type="button"
+          aria-label="Mobile menu"
+          aria-expanded={menuOpening}
+          aria-controls="mobile-menu"
+          use:menuButtonAction
+        >
+          <span class="group-aria-expanded:-mb-[0.1875rem] group-aria-expanded:rotate-45"></span>
+          <span class="group-aria-expanded:hidden"></span>
+          <span class="group-aria-expanded:-rotate-45"></span>
+        </button>
+        <div
+          class="absolute inset-0 hidden items-start justify-center overflow-y-scroll bg-[hsla(0,0%,0%,0.5)] pt-24 pb-6 peer-aria-expanded:flex md:static md:block md:w-full md:overflow-auto md:bg-transparent md:p-0"
+          id="mobile-menu"
+          use:menuAction
+        >
+          <div class="w-[min(100vw-3rem,25rem)] rounded-lg bg-purple-950 px-6 py-10 md:w-full md:bg-transparent md:p-0">
+            <ul
+              class="flex h-fit flex-col gap-8 text-center text-lg leading-[1.5625rem] font-bold text-white *:shrink-0 *:transition-colors md:flex-row md:items-center md:pl-[2.8125rem] md:text-[0.9375rem] md:leading-none md:text-gray-500 md:*:hover:text-gray-950"
+              role="list"
+            >
+              <li><a href="/">Features</a></li>
+              <li><a href="/">Pricing</a></li>
+              <li><a href="/">Resources</a></li>
+              <li class="border-b border-gray-500 md:grow md:border-none" role="none"></li>
+              <li><a href="/">Login</a></li>
+              <li>
+                <Button
+                  link="/"
+                  xClass=" pt-4 pb-[0.875rem] text-lg md:text-[0.9375rem] leading-none md:pt-3 md:pb-[0.8125rem] md:px-6"
+                >
+                  Sign Up
+                </Button>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      </nav>
     </div>
   </Container>
-</nav>
+</header>
