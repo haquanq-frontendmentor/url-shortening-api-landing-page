445565d7ccfa6786ae109c9f87784436c81d0367
Ha Quanq <quangha0120010@gmail.com>
2025-10-11 09:30:52 +0700

    Ignore svelte a11y invalid attribute warning

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [tailwindcss(), svelte()],
+    plugins: [
+        tailwindcss(),
+        svelte({
+            onwarn: (warning, defaltHandler) => {
+                if (warning.code === "a11y_invalid_attribute") return;
+                defaltHandler(warning);
+            },
+        }),
+    ],
     base: "/url-shortening-api-landing-page/",
 });
